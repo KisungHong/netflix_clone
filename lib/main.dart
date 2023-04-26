2a99efe47333da46a051a02dfd594a4b3a13a846
KisungHong <95329249+KisungHong@users.noreply.github.com>
2023-04-26 22:55:11 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HongFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Text('home'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('saved contents'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('list'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
