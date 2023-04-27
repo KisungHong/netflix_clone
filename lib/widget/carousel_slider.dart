77744dfa8133a78c50ba6289e5599d80725fa774
KisungHong <95329249+KisungHong@users.noreply.github.com>
2023-04-27 12:44:11 +0900

    오류해결

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
 
 class CarouselImage extends StatefulWidget {
-  final List<Movie> movies;
+  final List<Movie>? movies;
   const CarouselImage({
     Key? key,
     required this.movies,
@@ -16,21 +16,21 @@ class CarouselImage extends StatefulWidget {
 }
 
 class _CarouselImageState extends State<CarouselImage> {
-  List<Movie> movies;
-  List<Widget> images;
-  List<String> keywords;
-  List<bool> likes;
+  List<Movie>? movies;
+  List<Widget>? images;
+  List<String>? keywords;
+  List<bool>? likes;
   int _currentPage = 0;
-  String _currentKeyword;
+  late String _currentKeyword;
 
   @override
   void initState() {
     super.initState();
     movies = widget.movies;
-    images = movies.map((m) => Image.asset('./images' + m.poster)).toList();
-    keywords = movies.map((m) => m.keyword).toList();
-    likes = movies.map((m) => m.like).toList();
-    _currentKeyword = keywords[0];
+    images = movies?.map((m) => Image.asset(m.poster)).toList();
+    keywords = movies?.map((m) => m.keyword).toList();
+    likes = movies?.map((m) => m.like).toList();
+    _currentKeyword = keywords![0];
   }
 
   @override
@@ -46,7 +46,7 @@ class _CarouselImageState extends State<CarouselImage> {
             onPageChanged: (index, reason) {
               setState(() {
                 _currentPage = index;
-                _currentKeyword = keywords[_currentPage];
+                _currentKeyword = keywords![_currentPage];
               });
             },
           ),
