a20b60eb88d36d1bd1763af92a491df3f1d0f106
KisungHong <95329249+KisungHong@users.noreply.github.com>
2023-05-01 17:45:59 +0900

    List<bool> 문제해결 : null check 삽입

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -28,106 +28,109 @@ class _CarouselImageState extends State<CarouselImage> {
     super.initState();
     movies = widget.movies;
     images = movies?.map((m) => Image.asset(m.poster)).toList();
-    keywords = movies?.map((m) => m.keyword).toList();
-    likes = movies?.map((m) => m.like).toList();
+    keywords = movies?.map((m) => m.keyword).cast<String>().toList();
+    likes = movies?.map((m) => m.like).cast<bool>().toList();
     _currentKeyword = keywords![0];
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(20),
-        ),
-        CarouselSlider(
-          items: images,
-          options: CarouselOptions(
-            onPageChanged: (index, reason) {
-              setState(() {
-                _currentPage = index;
-                _currentKeyword = keywords![_currentPage];
-              });
-            },
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
-          child: Text(
-            _currentKeyword,
-            style: TextStyle(fontSize: 11),
+          CarouselSlider(
+            items: images,
+            options: CarouselOptions(
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentPage = index;
+                  _currentKeyword = keywords![_currentPage];
+                });
+              },
+            ),
           ),
-        ),
-        Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    likes![_currentPage]
-                        ? IconButton(onPressed: () {}, icon: Icon(Icons.check))
-                        : IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {},
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      likes![_currentPage]
+                          ? IconButton(
+                              onPressed: () {}, icon: Icon(Icons.check))
+                          : IconButton(
+                              icon: Icon(Icons.add),
+                              onPressed: () {},
+                            ),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.white)),
+                      onPressed: () {},
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
                           ),
-                    Text(
-                      '내가 찜한 콘텐츠',
-                      style: TextStyle(fontSize: 11),
-                    )
-                  ],
+                          Padding(
+                            padding: EdgeInsets.all(3),
+                          ),
+                          Text(
+                            '재생',
+                            style: TextStyle(color: Colors.black),
+                          )
+                        ],
+                      )),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.only(right: 10),
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.white)),
-                    onPressed: () {},
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.play_arrow,
-                          color: Colors.black,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(3),
-                        ),
-                        Text(
-                          '재생',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    )),
-              ),
-              Container(
-                padding: EdgeInsets.only(right: 10),
-                child: Column(
-                  children: <Widget>[
-                    IconButton(onPressed: () {}, icon: Icon(Icons.info)),
-                    Text(
-                      '정보',
-                      style: TextStyle(fontSize: 11),
-                    )
-                  ],
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: <Widget>[
+                      IconButton(onPressed: () {}, icon: Icon(Icons.info)),
+                      Text(
+                        '정보',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: makeIndicator(likes, _currentPage),
-                ),
-              )
-            ],
+              ],
+            ),
           ),
-        )
-      ]),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: makeIndicator(likes!, _currentPage),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-List<Widget> makeIndicator(List<Widget> list, int _currentPage) {
+List<Widget> makeIndicator(List list, int _currentPage) {
   List<Widget> result = [];
   for (var i = 0; i < list.length; i++) {
     result.add(Container(
